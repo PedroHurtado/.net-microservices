bee5885e1ec385cdafcb22563c1259db466c2acc
Pedro Hurtado <pedrohurt@gmail.com>
2025-10-28 14:21:30 +0100

    repository

--- common/infrastructure/Repository.cs
+++ common/infrastructure/Repository.cs
@@ -1,20 +1,34 @@
 namespace webapi.common.infrastructure;
+
 public interface IAdd<in T>
 {
     Task AddAsync(T entity, CancellationToken cancellationToken = default);
 }
 
-public interface IGet<T, in TId>
-{
-    Task<T?> GetAsync(TId id, CancellationToken cancellationToken = default);
+public interface IGet<T, in ID>{
+    Task<T?> GetAsync(ID id, CancellationToken cancellationToken = default);   
 }
 
-public interface IUpdate<in T, in TId> : IGet<T, TId>
+public interface IUpdate<T, in ID> 
 {
-    Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+    Task UpdateAsync(ID id,T entity, CancellationToken cancellationToken = default);
 }
 
-public interface IRemove<T, in TId> : IGet<T, TId>
+public interface IRemove<in ID> 
 {
-    Task RemoveAsync(TId id, CancellationToken cancellationToken = default);
+    Task RemoveAsync(ID id, CancellationToken cancellationToken = default);
 }
+
+/*
+public static class RepositoryExtensions
+{
+    public static async Task<T> GetOrThrowAsync<T, ID>(
+        this DbContext context,
+        ID id,
+        CancellationToken cancellationToken = default) where T : class
+    {
+        var entity = await context.Set<T>().FindAsync(new object[] { id! }, cancellationToken);
+        return entity ?? throw new KeyNotFoundException($"Entity with ID '{id}' not found.");
+    }
+}
+*/
