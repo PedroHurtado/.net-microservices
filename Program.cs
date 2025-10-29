0617c126f7b07abfcb0b953c8d79f58f8bbbd09c
Pedro Hurtado <pedrohurt@gmail.com>
2025-10-29 11:14:25 +0100

    configure swagger

--- Program.cs
+++ Program.cs
@@ -9,7 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.CustomSchemaIds(type => 
+    {
+        if (type.DeclaringType != null)        {
+            
+            return $"{type.DeclaringType.Name}.{type.Name}";
+        }       
+        return type.FullName?.Replace("+", ".").Replace(".", "") ?? type.Name;
+    });
+});
 builder.Services.AddInjectables();
 builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
