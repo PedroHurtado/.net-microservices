28204f1bf039621efc75adfba51bdcd7e530750f
Pedro Hurtado <pedrohurt@gmail.com>
2025-10-29 11:45:21 +0100

    create ingredient

--- features/ingredient/commands/CreateIngredient.cs
+++ features/ingredient/commands/CreateIngredient.cs
@@ -9,17 +9,30 @@ public class CreateIngredient : IFeatureModule
     public record struct Response(Guid Id,string Name, decimal Cost);
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPost("/ingredientes", async ([FromBody]Request request) =>
+        app.MapPost("/ingredientes", async ([FromBody] Request request) =>
         {
             var response = new Response(
                 Guid.NewGuid(),
                 request.Name,
                 request.Cost
             );
-            
+
             return Results.Ok(response);
         })
         .WithName("CreateIngredient")
         .WithOpenApi();
+    }
+
+    public interface IService
+    {
+        Task<Response> Handler(Request req);
+    }
+
+    public class Service : IService
+    {
+        public Task<Response> Handler(Request req)
+        {
+            throw new Exception();
+        }
     }
 }
