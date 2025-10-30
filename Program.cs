f6bf351d0801a899dcb338903dad56fa9375ec54
Pedro Hurtado <pedrohurt@gmail.com>
2025-10-30 09:26:03 +0100

    remove record from program

--- Program.cs
+++ Program.cs
@@ -117,7 +117,3 @@ app.MapFeatures();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
